80657a82aad850bbaae41de37c76a121fde8893e
Zeeshan Mohammed <zeeshan.mohammed.main@gmail.com>
2023-11-26 17:05:14 +0000

    Temporary Fixups

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import colors_json from './colors.json' assert {type:'json'};
+import fs from 'fs';
+var colors_json = JSON.parse(fs.readFileSync('./colors.json', 'utf8'));
 
 function dist_3d(x1, y1, z1, x2, y2, z2) {
     // First calculate the distance on the 2d plane
@@ -10,7 +11,7 @@ function dist_3d(x1, y1, z1, x2, y2, z2) {
 function find_closest_color(r, g, b) {
     // Iterate through the list of colors performing distance checks until we get the closest
     var closest_dist = Infinity;
-    var closest_field = {"name":"undefined","hex":"#000000","rgb":[0,0,0]};
+    var closest_field = {'name':'undefined','hex':'#000000','rgb':[0,0,0]};
     
     for (var i = 0; i < colors_json.length; i++) {
         var dist = dist_3d(r, g, b, colors_json[i]['rgb'][0], colors_json[i]['rgb'][1], colors_json[i]['rgb'][2]);
